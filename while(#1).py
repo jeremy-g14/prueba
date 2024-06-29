81a14f999c5c6f4dbc662e1fccf8dd70654f8976
jeremy-g14 <jeremygonzalez1144@gmail.com>
2024-06-28 20:09:32 -0500

    este es mi primer archivo

---
+++ while(#1).py
@@ -0,0 +1,10 @@
+contador= 0
+acumulador= 0
+
+while contador <= 3:
+    print(contador)
+    contador = contador + 1
+    acumulador = acumulador + contador
+    
+print(f"contador: {contador}")
+print(f"acumulador: {acumulador}")
